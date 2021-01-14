605777480b7b1a53a621f55c9a5ca62340407ed0
Sebastian Lember <sebi.lember@gmx.de>
2021-01-14 13:50:58 +0100

    NEXT-7997 - Add dropdown display for variant properties, fixes shopwareBoostDay/platform#229

--- src/Storefront/Resources/app/storefront/src/plugin/variant-switch/variant-switch.plugin.js
+++ src/Storefront/Resources/app/storefront/src/plugin/variant-switch/variant-switch.plugin.js
@@ -14,12 +14,14 @@ export default class VariantSwitchPlugin extends Plugin {
     static options = {
         url: '',
         elementId: '',
-        radioFieldSelector: '.product-detail-configurator-option-input'
+        radioFieldSelector: '.product-detail-configurator-option-input',
+        selectFieldSelector: '.product-detail-configurator-select-input'
     };
 
     init() {
         this._httpClient = new HttpClient();
-        this._radioFields = DomAccess.querySelectorAll(this.el, this.options.radioFieldSelector);
+        this._radioFields = DomAccess.querySelectorAll(this.el, this.options.radioFieldSelector, false);
+        this._selectFields = DomAccess.querySelectorAll(this.el, this.options.selectFieldSelector, false);
         this._elementId = this.options.elementId;
 
         this._ensureFormElement();
@@ -45,13 +47,15 @@ export default class VariantSwitchPlugin extends Plugin {
      * @private
      */
     _preserveCurrentValues() {
-        Iterator.iterate(this._radioFields, field => {
-            if (VariantSwitchPlugin._isFieldSerializable(field)) {
-                if (field.dataset) {
-                    field.dataset.variantSwitchValue = field.value;
+        if(this._radioFields) {
+            Iterator.iterate(this._radioFields, field => {
+                if (VariantSwitchPlugin._isFieldSerializable(field)) {
+                    if (field.dataset) {
+                        field.dataset.variantSwitchValue = field.value;
+                    }
                 }
-            }
-        });
+            });
+        }
     }
 
     /**
@@ -114,13 +118,24 @@ export default class VariantSwitchPlugin extends Plugin {
      */
     _getFormValue() {
         const serialized = {};
-        Iterator.iterate(this._radioFields, field => {
-            if (VariantSwitchPlugin._isFieldSerializable(field)) {
-                if (field.checked) {
-                    serialized[field.name] = field.value;
+        if(this._radioFields) {
+            Iterator.iterate(this._radioFields, field => {
+                if (VariantSwitchPlugin._isFieldSerializable(field)) {
+                    if (field.checked) {
+                        serialized[field.name] = field.value;
+                    }
                 }
-            }
-        });
+            });
+        }
+
+        if(this._selectFields) {
+            Iterator.iterate(this._selectFields, field => {
+                if (VariantSwitchPlugin._isFieldSerializable(field)) {
+                    const selectedOption = [...field.options].find(option => option.selected);
+                    serialized[field.name] = selectedOption.value;
+                }
+            });
+        }
 
         return serialized;
     }
